7bb3492732d1e8777e8a0505c6a86f2b73c14a2e
Four <aimzfatima08@gmail.com>
2022-07-13 13:13:07 +0530

    updated to Bit-Shopper

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -81,7 +81,7 @@ const Navbar = () => {
           </SearchContainer>
         </Left>
         <Center>
-          <Logo>LAMA.</Logo>
+          <Logo>Bit-Shopper.</Logo>
         </Center>
         <Right>
           <Link to="/register">
