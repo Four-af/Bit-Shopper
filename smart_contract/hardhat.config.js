d299542ea79d62e24f2bd5ed07899cca20055961
Four-af <aimzfatima08@gmail.com>
2022-07-29 04:30:35 +0000

    sol

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-waffle');
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: '0.8.9',
+  networks: {
+    goerli: {
+      // test network https key
+      url: 'https://eth-goerli.g.alchemy.com/v2/oJkP9HjycNvuXdOw4iXuKpTC5L6yTQPR',
+      // your account's private key
+      accounts: ['683533ef663b1194efc15bdf1322dfec3804c88c25e0e5ad51fbfb4602b93b97'],
+    },
+  },
 };
